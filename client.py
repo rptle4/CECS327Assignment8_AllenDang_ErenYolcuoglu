af31b1b8f5a1ca2db44ffaf6c0ef99ac641724c4
ErenAffetmez <eren.yolcuoglu01@student.csulb.edu>
2024-12-02 13:50:51 -0800

    Changed server and client
    Changed server and client

--- client.py
+++ client.py
@@ -1,34 +1,35 @@
 import socket
 
+# Prompt the user to enter the IP address and port number of the server
+server_ip = input("Enter the IP address of the server: ") #this will take input from comman line as prompted in the assignment 
+server_port = int(input("Enter the port number of the server: ")) #will take port number
 
-def run_client():
-    server_ip = input("Enter the public IP address of the server: ")
-    server_port = int(input("Enter the server port number: "))
+while True: #as requested doing the while loop so it will keep asking untill we exit
+    # Creating a TCP/IP socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    try:
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creates tcp socket
-        client_socket.connect((server_ip, server_port)) #connects to server
-        print(f"Connected to server: {server_ip} on port: {server_port}")
+    try:#using a try catch error since prof asked if there will be error response error happened.
+        # Connect the socket to the server's IP address and port number
+        server_address = (server_ip, server_port)
+        client_socket.connect(server_address)
 
-        while True:
-            message = input("Enter a message to send to the server (enter 'exit' to quit): ")
-            if not message:
-                print("No input, exiting and closing program") #if no input, exits program
-                break
-            if message.lower() == 'exit':
-                print("Exiting and closing program") #exits program
-                break
+        # # havind user type some message to the server once he connects
+        message = input("Enter a message to send to the server: ")
 
-            client_socket.sendall(message.encode()) #encodes and sends the message to the server
+        # Sending message in to the serever
+        client_socket.sendall(message.encode())
 
-            response = client_socket.recv(1024)
-            print(f"Server's reply: {response.decode()}") #decodes the response
+        # Receive the response from the server and print it
+        data = client_socket.recv(1024)
+        # print("Server response: {}".format(data.decode()))
 
-    except ConnectionError:
-        print("Failed to connect to server, try again")
+        # gets the message from the server and will decode and print 
+        data = client_socket.recv(1024)
+        print("Server: {}".format(data.decode()))
+
+    except Exception as e: #this will throw an error as i told earlier 
+        print("Error occurred: {}".format(e))
+
     finally:
-        client_socket.close() #closes socket
-
-
-if __name__ == "__main__":
-    run_client()
+        # Clean up the socket
+        client_socket.close()
--- server.py
+++ server.py
@@ -1,36 +1,50 @@
 import socket
 
+# Creating a TCP/IP socket to locate
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def run_server():
-    server_port = int(input("Enter the port number for the server: "))
+# taking the user's input to enter the IP address and port number of the server
+server_ip = input("Enter the IP address of the server: ") 
+server_port = int(input("Enter the port number of the server: "))
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#create tcp socket
+# Bind the socket to the specified IP address and port number
+server_address = (server_ip, server_port)
+server_socket.bind(server_address)
 
-    server_socket.bind(('0.0.0.0', server_port)) #socket binded to port
+# Listen for incoming connections trying to add the code TCp as professor discussed in the class and in the slides.
+server_socket.listen(1)
+print("Server is running on IP address {} and port number {}".format(server_ip, server_port))
 
-    server_socket.listen(1)#listening for connections allowing only 1 client to connect
-    print(f"Server port: {server_port} is now listening")
+#creating the while loop so it will keep asking untill we close/.
+while True:
+    # Wait for a connection untill someone joins from client
+    connection, client_address = server_socket.accept()
+    print("Connection from", client_address)
+    try:#using a try catch error since prof asked if there will be error response error happened.
+        
 
-    connection, client_address = server_socket.accept() #accepts connection from client
-    print(f"Connected to: {client_address}")
-
-    try:
+        # Receive the data in small chunks and convert to capital letters
         while True:
-            data = connection.recv(1024)#recieve message from client
-            if not data:
+            data = connection.recv(16)
+            
+            if data:
+                data = data.upper()
+                print("Client: {}".format(data.decode()))
+                # Send the capitalized data back to the client we will print the data we recieve from clients.
+                connection.sendall(data)
+
+                # havind user type some message to the client once he receives
+                message = input("Enter a message to send to the client: ")
+                message = message.upper()
+                # completing the task to do "UPPER CASE" and then send it to clients.
+                connection.sendall(message.encode())
+
+            else:
                 break
 
-            print(f"Message received from client: {data.decode()}")
-            response = data.decode().upper() #change the client's message to be uppercase
-
-            connection.sendall(response.encode()) #encodes and sends message back to client
-
-    except Exception as error:
-        print(f"ERROR: {error}")
+    except Exception as e:#if we get error it will throw an error while doing try and exception
+        print("Error occurred: {}".format(e))
 
     finally:
-        connection.close() #closes socket
-        print("Connection with client closed. Exiting program.")
-
-if __name__ == "__main__":
-    run_server()
+        # Cleaning up the connection
+        connection.close()
